3c6c4ffd2e8b5eeaf65d8320fc759a04eaf54025
Tokkojin <Tokkojin@gmail.com>
2015-01-15 15:12:12 -0500

    Card Constructor

--- app/src/main/java/lelee/cardsaganistandroid/Card.java
+++ app/src/main/java/lelee/cardsaganistandroid/Card.java
@@ -4,4 +4,13 @@ package lelee.cardsaganistandroid;
  * Created by tomatocurry1 on 1/13/2015.
  */
 public class Card {
+    String Type;
+    String Text;
+
+    Public Card(String type, String text)
+    {
+        Type = type;
+        Text = text;
+    }
+
 }
